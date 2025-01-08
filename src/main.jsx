d928396da911384c9850493fca8ffa9c3d0ba7fe
Stefano Casasola <ste.casasola@gmail.com>
2025-01-08 10:59:25 +0100

    Adds navbar

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,8 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+// tolgo questo import CSS perchè utilizzeremo bootstrap
+// import './index.css'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+createRoot(document.getElementById('root')).render(<App />)
+
+// ricordiamoci di eliminare la StrictMode di React!
