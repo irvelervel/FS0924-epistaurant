43d5aca5016bbc3072847a4399c132fe73e454e1
Stefano Casasola <ste.casasola@gmail.com>
2025-01-10 10:56:32 +0100

    End of season cliffhanger!

--- src/components/AdminSection.jsx
+++ src/components/AdminSection.jsx
@@ -7,12 +7,63 @@ import { Container, Row, Col, ListGroup } from 'react-bootstrap'
 
 // quando intendente recuperare dati da una API in un componente, dovete all'interno dello stato predisporre un posticino per ospitarli!
 
+const URL = 'https://striveschool-api.herokuapp.com/api/reservation'
+
 class AdminSection extends Component {
   state = {
     reservations: [], // inizializzo reservations come array vuoto in quanto intendo preservare il TIPO del dato; questa proprietà sarà SEMPRE un array, nel caso peggiore rimarrà vuoto oppure si riempirà di n elementi
   }
 
+  getReservationsThenCatch = () => {
+    // questa funzione si occuperà di recuperare le prenotazioni e di salvarle nello stato del componente; una volta fatto questo, l'interfaccia riempirà la lista delle prenotazioni automaticamente, perchè è già istruita a ciclare l'array reservations nello stato e a creare un elemento di lista per ognuno degli oggetti al suo interno
+    fetch(URL)
+      .then((response) => {
+        // finale buono
+        if (response.ok) {
+          // siamo sicuri che i dati sono arrivati
+          // visto che a noi in questo caso interessa ottenere il JSON
+          // delle Response ottenuta, invochiamo il metodo .json() di JavaScript che ci restituirà il contenuto della Response
+          return response.json()
+        } else {
+          // c'è stato un problema nella chiamata
+          throw new Error('Errore nella chiamata API')
+        }
+      })
+
+      .then((data) => {
+        // finale buono dell'estrazione del JSON
+        // data sarà l'array delle prenotazioni!
+        console.log('DATA', data)
+      })
+      .catch((error) => {
+        // finale cattivo
+        // c'è stato un problema di connessione, internet, etc.
+        console.log('ERRORE', error)
+      })
+  }
+
+  getReservationsAsyncAwait = async () => {
+    try {
+      const response = await fetch(URL)
+      if (response.ok) {
+        const data = await response.json()
+        console.log('DATA', data)
+        // ora che ho ottenuto l'array delle prenotazioni (data),
+        // vado a farci un setState in modo da riempire l'array vuoto
+        // chiamato "reservations" all'interno dello stato
+        this.setState({
+          reservations: data,
+        })
+      } else {
+        throw new Error('Errore nel recupero dati API')
+      }
+    } catch (error) {
+      console.log('ERRORE', error)
+    }
+  }
+
   render() {
+    this.getReservationsAsyncAwait()
     return (
       <Container>
         <Row className="justify-content-center my-3">
